fbb0e03c128e70ee68db4ec8576875bcd638fc22
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2021-02-23 00:18:52 +0200

    fix(lint): jest config issues

--- test/pages/index.test.js
+++ test/pages/index.test.js
@@ -1,11 +1,11 @@
 // import { get, setupTest } from '@nuxt/test-utils'
 
-describe('index', () => {
+describe("index", () => {
   // setupTest({ server: true })
 
-  it('renders the index page', async () => {
+  it("renders the index page", () => {
     // const { body } = await get('/')
     // expect(body).toContain('<h1>Pages List</h1>')
     expect(true).toBe(true);
-  })
-})
+  });
+});
