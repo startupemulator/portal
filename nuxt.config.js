f8ef30f9dd264d1d4928a97c53dcf22207692e64
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2021-02-22 16:16:32 +0200

    chore(config): change router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,56 +1,52 @@
 export default {
-    // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        title: 'StartupEmulator',
-        htmlAttrs: {
-            lang: 'en'
-        },
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'StartupEmulator',
+    htmlAttrs: {
+      lang: 'en'
     },
-    target: 'static', // default is 'server'
-    generate: {
-        crawler: false
-    },
-
-    // Global CSS: https://go.nuxtjs.dev/config-css
-    css: ['assets/css/main.scss'],
-
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [],
-
-    // Auto import components: https://go.nuxtjs.dev/config-components
-    components: true,
-
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-    buildModules: [],
-
-    // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [
-        // https://go.nuxtjs.dev/axios
-        '@nuxtjs/axios',
-        '@nuxtjs/strapi'
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
     ],
-    strapi: {
-      url: process.env.STRAPI_URL || 'http://localhost:1337'
-    },
-    // Axios module configuration: https://go.nuxtjs.dev/config-axios
-    axios: {},
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  target: 'static', // default is 'server'
+  generate: {
+    crawler: false
+  },
+  router: {
+    base: '/'
+  },
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['@assets/css/main.scss'],
 
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
 
-    router: {
-        base: './',
-        mode: 'hash'
-    },
-    // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {
-        publichPatch: '/nuxt/'
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
 
-    }
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/strapi'
+  ],
+  strapi: {
+    url: process.env.STRAPI_URL || 'http://localhost:1337'
+  },
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+
+  }
 }
---
+++ pages/landing.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <app-header></app-header>
+    <app-get-experience></app-get-experience>
+    <app-startups-block></app-startups-block>
+    <app-challenges-block></app-challenges-block>
+    <app-team-develop></app-team-develop> 
+    <app-take-part></app-take-part>
+    <app-top-startups></app-top-startups> 
+    <app-practicants></app-practicants>
+    <app-footer> </app-footer> 
+
+    
+</div>
+</template>
+
+<script>
+import AppHeader from '@/components/appHeader.vue'
+import AppGetExperience from '@/components/homePage/appGetExperience.vue'
+import AppStartupsBlock from '@/components/homePage/appStartupsBlock.vue'
+import AppChallengesBlock from '@/components/homePage/appChallengesBlock.vue'
+import AppTeamDevelop from '~/components/homePage/appTeamDevelop.vue'
+import AppTakePart from '~/components/homePage/appTakePart.vue'
+import AppTopStartups from '~/components/homePage/appTopStartups.vue'
+import AppPracticants from '~/components/homePage/appPracticants.vue'
+import AppFooter from '~/components/appFooter.vue'
+
+export default {
+    components:{
+        AppHeader,
+        AppGetExperience,
+        AppStartupsBlock,
+        AppChallengesBlock,
+        AppTeamDevelop,
+        AppTakePart,
+        AppTopStartups,
+        AppPracticants,
+        AppFooter
+    }
+}
+</script>
+
+<style >
+
+
+</style> 
