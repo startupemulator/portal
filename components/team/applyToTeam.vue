ad7cd7b0b239eb9eb10d62ea6873eb1fb2e7e5f7
vmwork <49744511+vmwork@users.noreply.github.com>
2021-03-03 15:45:44 +0200

    fix(startups block): in the card 'startup' block 'technologies' no more 2 lines

--- components/team/applyToTeam.vue
+++ components/team/applyToTeam.vue
@@ -1,168 +1,20 @@
 <template>
   <div class="apply-to-team">
-    <nuxt-link v-if="step" to="/"
-      ><button type="button" class="apply-to-team-btn-back">
-        <img src="@/assets/img/arrow.svg" alt="arrow" /> Back
-      </button></nuxt-link
-    >
-    <button
-      v-else
-      type="button"
-      class="apply-to-team-btn-back"
-      @click="changeStepOnApplyToTeam"
-    >
-      <img src="@/assets/img/arrow.svg" alt="arrow" /> Back
-    </button>
-    <transition name="slide-fade">
-      <div v-if="step" class="step-1">
-        <h2>Your experience</h2>
-        <p>Total years of your experience</p>
-        <ul class="step-1__experience">
-          <li
-            v-for="item in experienceYears"
-            :key="item.id"
-            class="experience__item"
-          >
-            <input v-show="false" :id="item.id" type="radio" name="years" />
-            <label :for="item.id">{{ item.title }}</label>
-          </li>
-        </ul>
-        <p>Pick technologies you have an experience with</p>
-
-        <ul class="step-1__experience">
-          <li
-            v-for="item in technologies"
-            :key="item.id"
-            class="experience-technologies_item"
-          >
-            <input
-              v-show="false"
-              :id="item.id + '-technologies'"
-              type="checkbox"
-            />
-            <label :for="item.id + '-technologies'">{{ item.title }}</label>
-          </li>
-        </ul>
-
-        <div class="add-technologi">
-          <ul class="step-1__add-technolodgy">
-            <li v-for="(item, i) in technology" :key="i" class="technolodgy">
-              <span>{{ item.name }}</span>
-              <button class="remove-technolodgy" @click="removeTechnology(i)">
-                <img src="@/assets/img/close.svg" alt="close" />
-              </button>
-            </li>
-
-            <input
-              class=""
-              type="text"
-              placeholder="Type a technology to add"
-              :value="technoName"
-              @keydown="addNewTechnolodgy($event)"
-            />
-          </ul>
-        </div>
-      </div>
-    </transition>
-    <transition name="slide-fade">
-      <div v-if="!step" class="step-2">
-        <h2>Apply to Startup #1</h2>
-        <div class="step-2__select">
-          <div class="step-2__custom-select">
-            <div class="custom-select" @click="openSelect">
-              <span>{{ selectedSpeciality }}</span>
-              <img
-                src="@/assets/img/arrow.svg"
-                alt="arrow"
-                :style="
-                  toggleSelect
-                    ? 'transform: rotate(-90deg); transition: 1s'
-                    : ''
-                "
-              />
-            </div>
-            <transition name="slide-up">
-              <ul v-show="toggleSelect" class="custom-select__list">
-                <li
-                  v-for="item in speciality"
-                  :key="item.id"
-                  class="custom-select__item"
-                  @click="selectSpeciality($event.target.textContent)"
-                >
-                  <span>{{ item.title }}</span>
-                </li>
-              </ul>
-            </transition>
-          </div>
-          <textarea placeholder="Comment"></textarea>
-        </div>
-
-        <transition name="slide-fade">
-          <div v-if="modalApplied" class="modal-applied">
-            <div class="applied-block">
-              <button type="button" class="applied__close">
-                <img
-                  src="@/assets/img/close.svg"
-                  alt="Close"
-                  @click="applyStartUp"
-                />
-              </button>
-
-              <h2>You successfully applied</h2>
-              <h3>
-                You will receive an email notification when Product owner
-                selects developer for a team. Also, you are able to apply to
-                other startups.
-              </h3>
-              <button type="button " class="discover-startups">
-                Discover More Startups
-              </button>
-            </div>
-          </div>
-        </transition>
-      </div>
-    </transition>
+    <U-back :link="'/'"></U-back>
+    <U-title :text="'Your experience'"></U-title>
+    <Duration-experiense-picker></Duration-experiense-picker>
   </div>
 </template>
-<script>
-import { mapState, mapActions } from "vuex";
 
+<script>
+import UBack from "~/components/theme/UBack";
+import UTitle from "~/components/theme/UTitle";
+import DurationExperiensePicker from "~/components/theme/DurationExperiensePicker";
 export default {
-  data: () => ({
-    itemSize: null,
-    technoName: "",
-    toggleSelect: false,
-    selectedSpeciality: "Select a speciality",
-  }),
-  computed: {
-    ...mapState([
-      "experienceYears",
-      "technologies",
-      "applyToTeam",
-      "speciality",
-      "technology",
-    ]),
-    step: (state) => state.applyToTeam.stepOne,
-    modalApplied: (state) => state.applyToTeam.applied,
-  },
-  methods: {
-    ...mapActions([
-      "changeStepOnApplyToTeam",
-      "applyStartUp",
-      "addTechnology",
-      "removeTechnology",
-    ]),
-    addNewTechnolodgy(e) {
-      this.addTechnology(e);
-      this.technoName = "";
-    },
-    openSelect() {
-      this.toggleSelect = !this.toggleSelect;
-    },
-    selectSpeciality(e) {
-      this.selectedSpeciality = e;
-      this.toggleSelect = !this.toggleSelect;
-    },
+  components: {
+    UBack,
+    UTitle,
+    DurationExperiensePicker,
   },
 };
 </script>
---
+++ components/theme/DurationExperiensePicker.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="technologi-picker">
+    <h2>{{ title }}</h2>
+    <form>
+      <Technologi-item
+        v-for="technologi in technologies"
+        :key="technologi.id"
+        :technologi-id="technologi.id"
+        :technologi-name="technologi.title"
+        :checked-class="technologi.checked ? 'checked' : ''"
+        :type="'radio'"
+        :name="'duration'"
+        @pickTechnologi="pickTechnologi($event, technologi.id)"
+      >
+        {{ technologi.title }}
+      </Technologi-item>
+    </form>
+  </div>
+</template>
+<script>
+import TechnologiItem from "./technologiItem.vue";
+export default {
+  components: {
+    TechnologiItem,
+  },
+  props: {
+    title: {
+      type: String,
+      default: " ",
+    },
+  },
+  data() {
+    return {
+      technologies: [
+        { id: 1, checked: false, title: "Less than 6 months" },
+        { id: 2, checked: false, title: "6 months – 1 year" },
+        { id: 3, checked: false, title: "1–2 years" },
+        { id: 4, checked: true, title: "2–3 years" },
+        { id: 5, checked: false, title: "3–4 years" },
+        { id: 6, checked: false, title: "4–5 years" },
+        { id: 7, checked: false, title: "More than 5 years" },
+      ],
+    };
+  },
+  methods: {
+    pickTechnologi(item, i) {
+      this.technologies.forEach((el) => {
+        if (i === el.id) {
+          el.checked = !el.checked;
+        } else if (i !== el.id) {
+          el.checked = false;
+        }
+      });
+    },
+  },
+};
+</script>
+<style lang="scss">
+.technologi-picker h2 {
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  margin-bottom: 13px;
+}
+</style>
