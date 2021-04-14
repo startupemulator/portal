648e1a97f11ba93ad4f8efb077c640b3473a4948
vmwork <49744511+vmwork@users.noreply.github.com>
2021-04-14 12:08:23 +0300

    refactor(delete startup-list): delete startup-list

--- pages/startups.vue
+++ pages/startups.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="startups-page">
-    <start-ups></start-ups>
+    <Startups :startups="startups" :technologies="technologies"></Startups>
   </div>
 </template>
-<script>
-import StartUps from "~/components/organisms/startups/startUps";
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
 
-export default {
+import Startups from "~/components/organisms/startups/startups.vue";
+
+@Component({
   components: {
-    StartUps,
+    Startups,
   },
-};
+})
+export default class extends Vue {
+  // data loaded here will be added during server rendering
+  async asyncData({ $strapi }) {
+    const startups = await $strapi.find("startups");
+    const technologies = await $strapi.find("technologies");
+    return {
+      startups,
+      technologies,
+    };
+  }
+}
 </script>
