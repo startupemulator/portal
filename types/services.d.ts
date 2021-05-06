bac0df8efbf5dd249f81f48483ad861ea74354ad
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2021-05-06 13:44:55 +0300

    style(types): disable eslint for types

--- types/services.d.ts
+++ types/services.d.ts
@@ -1,19 +1,16 @@
-import {Services} from '~/plugins/services';
+/* eslint-disable */
+import { Services } from "~/plugins/services";
 
 declare module "vue/types/vue" {
-  interface Vue extends Services {
-  }
+  interface Vue extends Services {}
 }
 
 declare module "@nuxt/types" {
-  interface NuxtAppOptions extends Services {
-  }
+  interface NuxtAppOptions extends Services {}
 
-  interface Context extends Services {
-  }
+  interface Context extends Services {}
 }
 
 declare module "vuex/types/index" {
-  interface Store<S> extends Services {
-  }
+  interface Store<S> extends Services {}
 }
