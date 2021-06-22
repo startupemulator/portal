f89d6f11743639e6adea0caed28abb10a7f3b64e
vmwork <49744511+vmwork@users.noreply.github.com>
2021-06-22 10:19:11 +0300

    fix(models profile.ts): models profile.ts

--- models/Profile.ts
+++ models/Profile.ts
@@ -4,10 +4,7 @@ import { Experience } from "./Experience";
 
 export interface Profile {
   id: string;
-<<<<<<< HEAD
-=======
   is_expert: boolean;
->>>>>>> main
   slug: string;
   user: Array<NotificationUser>;
   technologies: Array<Technology>;
